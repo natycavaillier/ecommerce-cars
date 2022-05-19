afb2f69eb4a8d96e38a275ca7a61dadd487a021a
luiz <luizhenrique201387@gmail.com>
2022-05-19 10:33:43 -0300

    Adicao da pagina comprar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComprarComponent } from './components/comprar/comprar.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  {path:'', component: HomeComponent }
+  {path:'', component: HomeComponent },
+  { path: 'comprar', component: ComprarComponent }
 ];
 
 @NgModule({
