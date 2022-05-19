fc884a1e18ec9137153b73e9bcd34f3ed8ab257c
lossath <augustajorgegugu@gmail.com>
2022-05-19 13:38:46 -0300

    Adição da pagina de feedback

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './components/cadastro/cadastro.component';
 import { ComprarComponent } from './components/comprar/comprar.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent },
   {path: 'comprar', component: ComprarComponent },
-  {path: 'cadastro', component: CadastroComponent}
+  {path: 'cadastro', component: CadastroComponent},
+  {path: 'feedback', component: FeedbackComponent}
+  
 ];
 
 @NgModule({
