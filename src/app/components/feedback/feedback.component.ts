d57dd8234ce3691c8a26e41874ac4c2bc82dbd54
lossath <augustajorgegugu@gmail.com>
2022-05-20 09:58:31 -0300

    personalização do feedback

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+
+function onlySoulcode(control: AbstractControl): ValidationErrors | null {
+  if(Validators.email(control) === null && Validators.required(control) === null) {
+    if (!control.value.includes('@soulcodeacademy.org')) {
+      return { onlysoulcode: true };
+    }
+  }
+  return null;
+}
 
 @Component({
   selector: 'app-feedback',
@@ -7,9 +22,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
+
+  feddbackForm = this.fb.group({
+    nome: ['', [Validators.required, Validators.minLength(2)]],
+    email: ['', [Validators.required, Validators.email, onlySoulcode]],
+    feedback: ['', [Validators.required, Validators.maxLength(150)]],
+  });
+
+  get nome() {
+    return this.feedbackForm.get('nome');
+  }
+
+  get email() {
+    return this.feedbackForm.get('email');
+  }
+
+  get feddback() {
+    return this.feedbackForm.get('feedback');
+  }
+
+  onSubmit() {
+    alert('Obrigado por seu feedback!');
+    console.log(this.feedbackForm.value);
+  }
+
+  ngOnInit(): void {}
+}
 
   ngOnInit(): void {
   }
-
 }
