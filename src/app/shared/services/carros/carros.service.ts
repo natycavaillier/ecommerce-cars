c253c6a01c8e0161bf346033b8dfbc2f77b500e4
Natália Cavaillier <nataliaabreu2000@hotmail.com>
2022-05-20 11:12:43 -0300

    Ajuste de imagem

--- src/app/shared/services/carros/carros.service.ts
+++ src/app/shared/services/carros/carros.service.ts
@@ -8,7 +8,7 @@ export class CarrosService {
 
   private carros: Carro[] = [
     {
-      imagem: 'https://img.freepik.com/vetores-gratis/carro-sedan-azul-isolado-no-branco-vector_53876-67375.jpg?t=st=1653005126~exp=1653005726~hmac=fed3fdb5c03fc566e9efbf8290f5bef8afc052f764787e4c57220f49b2672215&w=826',
+      imagem: 'https://quatrorodas.abril.com.br/wp-content/uploads/2022/04/honda-civic-seda-e1650568274124.jpg',
       modelo: 'HONDA CIVIC',
       descricao: '1.5 16V TURBO GASOLINA TOURING 4P CVT',
       preco: 134990,
@@ -17,7 +17,7 @@ export class CarrosService {
       localizacao: 'Barueri, SP'
     },
     {
-      imagem: 'https://img.freepik.com/vetores-gratis/carro-sedan-azul-isolado-no-branco-vector_53876-67375.jpg?t=st=1653005126~exp=1653005726~hmac=fed3fdb5c03fc566e9efbf8290f5bef8afc052f764787e4c57220f49b2672215&w=826',
+      imagem: 'https://quatrorodas.abril.com.br/wp-content/uploads/2018/04/corolla_gli_2019.jpg?quality=70&strip=info',
       modelo: 'TOYOTA COROLLA',
       descricao: '2.0 VVT-IE FLEX ALTIS DIRECT SHIFT',
       preco: 149990,
@@ -26,7 +26,7 @@ export class CarrosService {
       localizacao: 'Salvador, BA'
     },
     {
-      imagem: 'https://img.freepik.com/vetores-gratis/carro-sedan-azul-isolado-no-branco-vector_53876-67375.jpg?t=st=1653005126~exp=1653005726~hmac=fed3fdb5c03fc566e9efbf8290f5bef8afc052f764787e4c57220f49b2672215&w=826',
+      imagem: 'https://guiaauto.com.br/wp-content/uploads/2021/08/Honda-Fit-PCD-2022-2-750x460.jpg',
       modelo: 'HONDA FIT',
       descricao: '1.5 LX 16V FLEX 4P AUTOMÁTICO',
       preco: 75500,
