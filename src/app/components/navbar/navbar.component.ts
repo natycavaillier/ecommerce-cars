f0985dd63e9a8215533284b532951dd8b2a5d32a
lossath <augustajorgegugu@gmail.com>
2022-05-21 13:37:00 -0300

    Mudar o nome Soul Cars para SoulCars na navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  nomeDoApp: String = 'Soul Cars';
+  nomeDoApp: String = 'SoulCars';
   isMenuCollapsed = true;
 
   constructor() { }
