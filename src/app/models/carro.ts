5d2048356bd19462b3a6495992318d3f35407503
luiz <luizhenrique201387@gmail.com>
2022-05-22 20:10:12 -0300

    Personalizacao pag. negociar, enums,

--- src/app/models/carro.ts
+++ src/app/models/carro.ts
@@ -7,4 +7,19 @@ export interface Carro {
     ano: string;
     kmRodados: number;
     localizacao: string;
+    cor: string;
+    finalPlaca: number;
+    marca: MarcaCarro;
+}
+
+export enum MarcaCarro {
+    VOLKSWAGEN = 'volkswagen',
+    HONDA = 'honda',
+    FIAT = 'fiat',
+    TOYOTA = 'toyota',
+    HYUNDAI = 'hyundai',
+    MITSUBISHI = 'mitsubishi',
+    FORD = 'ford',
+    CHEVROLET = 'chevrolet',
+    RENAULT = 'renault'
 }
--- src/app/shared/services/carros/carros.service.ts
+++ src/app/shared/services/carros/carros.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Carro } from 'src/app/models/carro';
+import { Carro, MarcaCarro } from 'src/app/models/carro';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +9,16 @@ export class CarrosService {
   private carros: Carro[] = [
     {
       id: 1,
-      imagem: 'https://quatrorodas.abril.com.br/wp-content/uploads/2022/04/honda-civic-seda-e1650568274124.jpg',
+      imagem: 'https://1.bp.blogspot.com/-63TSqz5WpzU/X8Amvcr9kKI/AAAAAAACzC4/DtJrroNty5UsFSJFkf81nriB9JJksr7QACLcBGAsYHQ/s1920/Honda%2BCivic%2B8.jpg',
       modelo: 'HONDA CIVIC',
       descricao: '1.5 16V TURBO GASOLINA TOURING 4P CVT',
       preco: 134990,
       ano: '2018/2019',
       kmRodados: 51300,
-      localizacao: 'Barueri, SP'
+      localizacao: 'Ribeirão Preto, SP',
+      cor: 'Preto',
+      finalPlaca: 2,
+      marca: MarcaCarro.HONDA
     },
     {
       id: 2,
@@ -25,68 +28,76 @@ export class CarrosService {
       preco: 149990,
       ano: '2020/2020',
       kmRodados: 13664,
-      localizacao: 'Salvador, BA'
+      localizacao: 'Salvador, BA',
+      cor: 'Prata',
+      finalPlaca: 9,
+      marca: MarcaCarro.TOYOTA
     },
     {
       id: 3,
-      imagem: 'https://guiaauto.com.br/wp-content/uploads/2021/08/Honda-Fit-PCD-2022-2-750x460.jpg',
+      imagem: 'https://wallpaperaccess.com/full/2145000.jpg',
       modelo: 'HONDA FIT',
       descricao: '1.5 LX 16V FLEX 4P AUTOMÁTICO',
       preco: 75500,
       ano: '2017/2017',
       kmRodados: 46072,
-      localizacao: 'São Paulo, SP'
+      localizacao: 'São Paulo, SP',
+      cor: 'Prata',
+      finalPlaca: 9,
+      marca: MarcaCarro.HONDA
     },
     {
       id: 4,
-      imagem: 'https://img.freepik.com/vetores-gratis/carro-sedan-azul-isolado-no-branco-vector_53876-67375.jpg?t=st=1653005126~exp=1653005726~hmac=fed3fdb5c03fc566e9efbf8290f5bef8afc052f764787e4c57220f49b2672215&w=826',
-      modelo: 'HONDA CIVIC',
-      descricao: '1.5 16V TURBO GASOLINA TOURING 4P CVT',
-      preco: 134990,
-      ano: '2018/2019',
-      kmRodados: 51300,
-      localizacao: 'Barueri, SP'
+      imagem: 'https://fotos.jornaldocarro.estadao.com.br/uploads/2017/08/16155825/33285004.jpg',
+      modelo: 'HYUNDAI CRETA',
+      descricao: '1.6 16V FLEX PULSE PLUS AUTOMÁTICO',
+      preco: 97990,
+      ano: '2019/2020',
+      kmRodados: 41900,
+      localizacao: 'Barueri, SP',
+      cor: 'Prata',
+      finalPlaca: 9,
+      marca: MarcaCarro.HONDA
     },
     {
       id: 5,
-      imagem: 'https://img.freepik.com/vetores-gratis/carro-sedan-azul-isolado-no-branco-vector_53876-67375.jpg?t=st=1653005126~exp=1653005726~hmac=fed3fdb5c03fc566e9efbf8290f5bef8afc052f764787e4c57220f49b2672215&w=826',
-      modelo: 'HONDA CIVIC',
-      descricao: '1.5 16V TURBO GASOLINA TOURING 4P CVT',
-      preco: 134990,
-      ano: '2018/2019',
-      kmRodados: 51300,
-      localizacao: 'Barueri, SP'
+      imagem: 'https://1.bp.blogspot.com/--xj77uV_t5s/W-HvjTyoQWI/AAAAAAAALQo/rNpS7MAqK_YUG_ZvPezuKhXn5oU-l1KmACLcBGAs/s1600/Renault%2BKwid%2BOutsider%2B001.jpg',
+      modelo: 'RENAULT KWID',
+      descricao: '1.0 12V SCE FLEX ZEN MANUAL',
+      preco: 45990,
+      ano: '2019/2020',
+      kmRodados: 33372,
+      localizacao: 'São Paulo, SP',
+      cor: 'Prata',
+      finalPlaca: 9,
+      marca: MarcaCarro.RENAULT
     },
     {
       id: 6,
-      imagem: 'https://img.freepik.com/vetores-gratis/carro-sedan-azul-isolado-no-branco-vector_53876-67375.jpg?t=st=1653005126~exp=1653005726~hmac=fed3fdb5c03fc566e9efbf8290f5bef8afc052f764787e4c57220f49b2672215&w=826',
-      modelo: 'HONDA CIVIC',
-      descricao: '1.5 16V TURBO GASOLINA TOURING 4P CVT',
-      preco: 134990,
-      ano: '2018/2019',
-      kmRodados: 51300,
-      localizacao: 'Barueri, SP'
+      imagem: 'https://cdn.motor1.com/images/mgl/J4p1g/s1/hyundai-hb20-1.0-sense-2021-motor1.webp',
+      modelo: 'HYUNDAI HB20',
+      descricao: '1.6 COMFORT PLUS 16V FLEX 4P MANUAL',
+      preco: 66500,
+      ano: '2019/2019',
+      kmRodados: 42800,
+      localizacao: 'Praia Grande, SP',
+      cor: 'Prata',
+      finalPlaca: 9,
+      marca: MarcaCarro.HYUNDAI
     },
     {
       id: 7,
-      imagem: 'https://img.freepik.com/vetores-gratis/carro-sedan-azul-isolado-no-branco-vector_53876-67375.jpg?t=st=1653005126~exp=1653005726~hmac=fed3fdb5c03fc566e9efbf8290f5bef8afc052f764787e4c57220f49b2672215&w=826',
-      modelo: 'HONDA CIVIC',
-      descricao: '1.5 16V TURBO GASOLINA TOURING 4P CVT',
-      preco: 134990,
-      ano: '2018/2019',
-      kmRodados: 51300,
-      localizacao: 'Barueri, SP'
+      imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDVSySJBRUJ1utuUmRVbffg0gNjKG5Uwx1Pg&usqp=CAU',
+      modelo: 'Honda Civic Type-R 2022',
+      descricao: '2.0L turbo, 320 cv a 6500 rpm, 40,8 kgfm a 4500 rpm',
+      preco: 184900,
+      ano: '2022/2022',
+      kmRodados: 300,
+      localizacao: 'Araraquara, SP',
+      cor: 'Beje',
+      finalPlaca: 9,
+      marca: MarcaCarro.HONDA
     },
-    {
-      id: 8,
-      imagem: 'https://img.freepik.com/vetores-gratis/carro-sedan-azul-isolado-no-branco-vector_53876-67375.jpg?t=st=1653005126~exp=1653005726~hmac=fed3fdb5c03fc566e9efbf8290f5bef8afc052f764787e4c57220f49b2672215&w=826',
-      modelo: 'HONDA CIVIC',
-      descricao: '1.5 16V TURBO GASOLINA TOURING 4P CVT',
-      preco: 134990,
-      ano: '2018/2019',
-      kmRodados: 51300,
-      localizacao: 'Barueri, SP'
-    }
   ];
 
   pushCarros(carro: Carro){
@@ -100,7 +111,6 @@ export class CarrosService {
   getCarro(id: number) {
     return this.carros[id - 1];
   }
-
 
   constructor() { }
 }
