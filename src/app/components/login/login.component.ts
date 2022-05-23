5614e69af2b939a42abfbbb66c62b0b1b0ac01b8
lossath <augustajorgegugu@gmail.com>
2022-05-22 23:29:57 -0300

    Aprimoramento da pagina login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, ValidationErrors, Validators } from '@angular/forms';
+
+function onlyCadastro(control: AbstractControl): ValidationErrors | null {
+  if (Validators.email(control) === null && Validators.required(control) === null) {
+    if(!control.value.includes("@gmail.com")) {
+      return {onlyCadastro: true};
+    }
+  }
+
+  return null;
+}
 
 @Component({
   selector: 'app-login',
@@ -7,7 +18,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email, onlyCadastro]],
+    senha: ['', [Validators.required, Validators.minLength(5)]],
+  });
+
+  get email () {
+    return this.loginForm.get('email');
+  }
+
+  get senha () {
+    return this.loginForm.get('senha');
+  }
+
+  onSubmit() {
+    this.loginForm.reset();
+    alert('Login feito com sucesso!');
+  }
 
   ngOnInit(): void {
   }
