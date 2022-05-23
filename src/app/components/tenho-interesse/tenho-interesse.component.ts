f24bc37d1c3c298a5c72113013abb271dd1ee138
Natália Cavaillier <nataliaabreu2000@hotmail.com>
2022-05-23 13:04:02 -0300

    Adição da pagina de Tenho Interesse e link na pagina Negociar

---
+++ src/app/components/tenho-interesse/tenho-interesse.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tenho-interesse',
+  templateUrl: './tenho-interesse.component.html',
+  styleUrls: ['./tenho-interesse.component.css']
+})
+export class TenhoInteresseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
